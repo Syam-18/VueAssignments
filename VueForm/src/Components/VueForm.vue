40cacaf69ea7861040becd59109dedaebe6dd322
Syam-18 <sukeshgrandhisila@gmail.com>
2025-07-24 15:36:56 +0530

    small changes

--- VueForm/src/Components/VueForm.vue
+++ VueForm/src/Components/VueForm.vue
@@ -10,13 +10,13 @@ const formRef = useTemplateRef('form')
 const errorMsg = ref('')
 const submitForm = (e) => {
   e.preventDefault();
-  if (name.value.trim === '') {
+  if (name.value.trim() === '') {
     errorMsg.value = '*Name field required';
   }
-  else if (mail.value.trim === '') {
+  else if (mail.value.trim() === '') {
     errorMsg.value = '*Mail field required';
   }
-  else if (age.value.trim === '') {
+  else if (age.value.trim() === '') {
     errorMsg.value = '*Age field required';
   }
   else {
@@ -52,6 +52,6 @@ const submitForm = (e) => {
     <input type="number" min="18" max="100" class="border-1 rounded w-[90%] px-4 py-1 m-2 mt-0" id="age" v-model="age" />
     <p class="text-red-700 mx-2" v-if="errorMsg">{{ errorMsg }}</p>
     <p class="text-green-400">{{successMsg}}</p>
-    <button class="bg-[#a2e0e3] text-black text-2xl m-2 size-min rounded px-2 py-1" @click="submitForm" disabled>Submit</button>
+    <button class="bg-[#a2e0e3] text-black text-2xl m-2 size-min rounded px-2 py-1" @click="submitForm" >Submit</button>
   </form>
 </template>
